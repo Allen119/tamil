ea22f73fda9494ff85961c44fb4ffd12204c5e74
Allen119 <allencijo119@gmail.com>
2025-09-01 00:43:12 +0530

    props

--- tamil/src/App.jsx
+++ tamil/src/App.jsx
@@ -2,14 +2,17 @@ import './App.css'
 import Course from './Course/Course'
 import Footer from './Footer'
 import NavBar from './NavBar'
+import html from './assets/react.svg';
+import css from './assets/css.png';
+
 
 function App() {
 
   return (
     <>
-      <NavBar/>
-      <Course/>
-      <Footer/>
+      <Course name = "HTML" price = "$199" image={html}/>
+      <Course name = "CSS" price = "$198" image={css}/>
+      <Course name = "JS"/>
     </>
   )
 }
--- tamil/src/Course/Course.jsx
+++ tamil/src/Course/Course.jsx
@@ -1,6 +1,7 @@
 // import styles from "./Course.module.css";
-const course1 = "HTML"
-function Course() {
+import html from "../assets/react.svg";
+// const course1 = "HTML"
+function Course({ name = "code IO course", image = html, price = 456 }) {
     const intStyle  = {
         backgroundColor : 'blue',
     }
@@ -8,12 +9,13 @@ function Course() {
         <div className='card'>
         {/* for index.css */}
         {/* <div className={styles.card}>  for external css */}
-            <img src="" alt="" />
-            <h3>{course1}</h3>
+            <img src={image} alt="" />
+            <h3>{name}</h3>
             {/* below is internal css */}
-            <p style={intStyle}>This is code io's HTML course</p>
+            <p style={intStyle}>{price}</p>
         </div>
     );
 }
+
 
 export default Course;
