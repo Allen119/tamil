3daff88cd4a1e0862c9926e362a67155f20eab89
Cijo <2440741@cognizant.com>
2025-09-04 07:10:51 +0530

    conditional rendering, filter sorting, click events

--- tamil/src/App.jsx
+++ tamil/src/App.jsx
@@ -1,18 +1,12 @@
 import './App.css'
-import Course from './Course/Course'
-import Footer from './Footer'
-import NavBar from './NavBar'
-import html from './assets/react.svg';
-import css from './assets/css.png';
+import CourseList from './CourseList'
 
 
 function App() {
 
   return (
     <>
-      <Course name = {88} price = "$199" image={html} rating = {4}/>
-      <Course name = "CSS" price = "$198" image={css} rating = {4.5}/>
-      <Course name = "JS" price = "$199" image={html} rating = "4.6" />
+      <CourseList/>
     </>
   )
 }
--- tamil/src/Course/Course.jsx
+++ tamil/src/Course/Course.jsx
@@ -1,9 +1,17 @@
 // import styles from "./Course.module.css";
-import html from "../assets/react.svg";
+// import html from "../assets/react.svg";
 import PropTypes from "prop-types";
 //npm install prop-types
 // const course1 = "HTML"
+
+
+
 function Course(props) {
+    function clickHandle(discount,event){
+    console.log(props.name, "button clicked",discount);
+    console.log("event object",event);
+    }
+
     const intStyle  = {
         backgroundColor : 'blue',
     }
@@ -16,6 +24,7 @@ function Course(props) {
             {/* below is internal css */}
             <p style={intStyle}>{props.price}</p>
             <span>{props.rating}</span>
+            <button onClick={(event) => clickHandle(20,event)}>Buy Now</button>
         </div>
     );
 }
